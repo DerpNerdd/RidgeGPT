5c3d3ae2e25be55201840e0c8c9b1b58bbeaf1b3
Alan Sanchez <sanchez1.alan1@gmail.com>
2024-11-18 09:27:36 -0700

    Delete .api directory
    Not needed as this is the old CHAI API and not ChatGPT API

--- .api/apis/chai-api/types.d.ts
+++
@@ -1,4 +0,0 @@
-import type { FromSchema } from 'json-schema-to-ts';
-import * as schemas from './schemas';
-export type CreateChatCompletionBodyParam = FromSchema<typeof schemas.CreateChatCompletion.body>;
-export type CreateChatCompletionResponse200 = FromSchema<typeof schemas.CreateChatCompletion.response['200']>;
